5cc6ec60ec948d7fe1aa234407687ad0a44876dc
zaryab444 <zaryabdeveloper61@gmail.com>
2020-03-10 04:35:03 -0700

    UserRegistration

---
+++ Userregistration/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Userregistration.Models;
+namespace Userregistration.Controllers
+{
+    public class UserController : Controller
+    {
+      [HttpGet]
+        public ActionResult ADDorEdit(int id = 0)
+        {
+            Userlog user = new Userlog();
+            return View(user);
+        }
+
+        [HttpPost]
+      public ActionResult ADDorEdit(Userlog usermodel)
+      {  using (AppDBEntities apmodel = new AppDBEntities()){
+          if (apmodel.Userlogs.Any(x => x.Username == usermodel.Username)){
+              ViewBag.DuplicateMessage = "Already exist";
+              return View("ADDorEdit",usermodel);
+
+
+          }
+          apmodel.Userlogs.Add(usermodel);
+          apmodel.SaveChanges();
+            }
+      ModelState.Clear();
+      ViewBag.sucessMessage = "Registration Completed";
+      return View("ADDorEdit", new Userlog());
+
+      }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+    
+    }
+}
